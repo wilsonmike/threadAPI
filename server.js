22646bfb9fb73cad6a748464a22cc3af4d8f37e0
mike <michaelwilsonsj@hotmail.com>
2022-06-14 22:05:57 -0400

    added dynamic route - querystring is legacy now lol

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const http = require('http')
+const querystring = require('querystring')
 
 const port = process.env.PORT || 1337
-
-const server = http.createServer(function (req, res) {
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify({ text: 'hi', numbers: [1, 2, 3] }))
-})
 
 function respondText(req, res) {
     res.setHeader('Content-Type', 'text/plain')
@@ -16,6 +12,42 @@ function respondJson(req, res) {
     res.setHeader('Content-Type', 'application/json')
     res.end(JSON.stringify({ text: 'hi', numbers: [1, 2, 3] }))
 }
+
+
+function respondNotFound(req, res) {
+    res.writeHead(404, { 'Content-Type': 'text/plain' })
+    res.end('Not Found')
+}
+
+function respondEcho(req, res) {
+    const { input = '' } = querystring.parse(
+        req.url
+            .split('?')
+            .slice(1)
+            .join('')
+    )
+
+    res.setHeader('Content-Type', 'application/json')
+    res.end(
+        JSON.stringify({
+            normal: input,
+            shouty: input.toUpperCase(),
+            characterCount: input.length,
+            backwards: input
+                .split('')
+                .reverse()
+                .join('')
+        })
+    )
+}
+
+const server = http.createServer(function (req, res) {
+    if (req.url === '/') return respondText(req, res)
+    if (req.url === '/json') return respondJson(req, res)
+    if (req.url.match(/^\/echo/)) return respondEcho(req, res)
+
+    respondNotFound(req, res)
+})
 
 
 server.listen(port)
